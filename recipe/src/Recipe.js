8f1360d41d319f90a6b821081ad1b2509f9b06b7
romasinha <sinha.roma99@gmail.com>
2020-07-04 00:59:12 +0530

    formatted

--- recipe/src/Recipe.js
+++ recipe/src/Recipe.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 const Recipe = ({title, calories, image, ingredients}) =>{
     return(
-        <div>
-            <h1>{title}</h1>
-            <ol>
+        <div className= "recipe">
+            <h1 className= "title">{title}</h1>
+            
+            <ul className = "list">
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
                 ))}
-            </ol>
-            <p>{calories}</p>
+            </ul>
             <img src = {image}></img>
+            <p><span> Calories : </span>{calories}</p>
+            
             
         </div>
     )
